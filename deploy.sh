d9f6c55d97ce9d69b342d8c3584c338bdecabae8
Ori <rotchildori@gmail.com>
2020-05-14 21:24:19 +0300

    first atteampt at lunch!

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+docker build -t orirothschild/multi-client:latest -t orirothschild/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t orirothschild/multi-server:latest -t orirothschild/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t orirothschild/multi-worker:latest -t orirothschild/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push orirothschild/multi-client:latest
+docker push orirothschild/multi-server:latest
+docker push orirothschild/multi-worker:latest
+
+docker push orirothschild/multi-client:$SHA
+docker push orirothschild/multi-server:$SHA
+docker push orirothschild/multi-worker:$SHA
+
+kubectl apply -f k8s 
+
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-pgfix-5-11
+kubectl set image deployments/client-deployment server=orirothschild/multi-client:$SHA
+kubectl set image deployments/worker-deployment server=orirothschild/multi-worker:$SHA
